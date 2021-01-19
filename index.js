317564256bd347a4edb85b4a9c4e25d4ff6e771b
AshishKumar409 <ashishkumarnath9@gmail.com>
2021-01-19 15:18:33 +0530

    index JS

---
+++ index.js
@@ -0,0 +1,25 @@
+
+let input = document.getElementById('User')
+let button = document.querySelector('.saveName')
+let warning = document.querySelector('.warning')
+
+
+let playerName;
+input.addEventListener('keyup',()=>{
+  playerName = input.value
+  button.disabled =!playerName
+  if(input.value===''){
+  warning.style.visibility = 'visible'
+}else{
+  warning.style.visibility = 'hidden'
+}
+})
+
+
+
+saveName=(e)=>{
+  e.preventDefault()
+  console.log('clicked');
+  localStorage.setItem('PlayerName',playerName)
+  window.location.assign('/game.html')
+}
